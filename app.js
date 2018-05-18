e15cc42ef6a0b177306d3b1354052f1d2baca40d
kbbushman <k.bushman@yahoo.com>
2018-05-18 07:54:54 -0700

    Initial commit, adds lab boilerplate

---
+++ app.js
@@ -0,0 +1,19 @@
+const simplehttp = new SimpleHTTP;
+
+// User Data
+const userData = {
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'jdoe@gmail.com'
+}
+
+// Get Users
+
+
+// Create  User
+
+
+// Update  User
+
+
+// Delete  User
---
+++ simplehttp.js
@@ -0,0 +1,28 @@
+class SimpleHTTP {
+  
+  constructor() {
+    this.newHTTP = new XMLHttpRequest();
+    
+  }
+
+  // GET REQUEST
+  get(url) {
+    
+  }
+
+  // POST REQUEST
+  post(url, data) {
+    
+  }
+
+  // PUT REQUEST
+  put(url, data) {
+    
+  }
+
+  // DELETE REQUEST
+  delete(url) {
+    
+  }
+
+}
