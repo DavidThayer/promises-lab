cf4256bfb2450ad4124d7077353630ce529f309b
David Thayer <david.thayer@gmail.com>
2018-05-18 12:02:22 -0700

    simple http request library

--- app.js
+++ app.js
@@ -7,13 +7,27 @@ const userData = {
   email: 'jdoe@gmail.com'
 }
 
+console.log(new XMLHttpRequest());
+
+// React import and export is from Babel
+
 // Get Users
+// simplehttp.get('https://jsonplaceholder.typicode.com/users')
+//   .then(res => console.log(res))
+//   .catch(res => console.log(err));
+
+// // Create  User
+// simplehttp.post('https://jsonplaceholder.typicode.com/users', userData)
+//   .then(res => console.log(res))
+//   .catch(res => console.log(err));
 
 
-// Create  User
-
-
-// Update  User
-
+// // Update  User
+// simplehttp.put('https://jsonplaceholder.typicode.com/users/2', userData)
+//   .then(res => console.log(res))
+//   .catch(res => console.log(err));
 
 // Delete  User
+simplehttp.delete('https://jsonplaceholder.typicode.com/users/2')
+  .then(res => console.log(res))
+  .catch(res => console.log(err));
